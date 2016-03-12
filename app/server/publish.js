7f2cddcd6af0d5a000ce994ff8b0fca1822de71d
Matthew Marcos <matthewmarcos94@gmail.com>
2016-03-12 16:55:39 +0800

    Add instructions

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,1 +1,20 @@
+/*
+    Add publications here as well as Meteor methods
 
+    @TODO: Add a check for flags
+
+    # Important!
+
+    ### Use lodash as often as possible!
+
+    ### Make sure that publications do the ff:
+    1. Check if user is logged in
+    2. Check if user is of correct type
+    3. Get username of teacher
+    4. Return only data that username should see.
+    5. Return only as little data as possible (bandwith issues)
+*/
+
+Meteor.publish('myClasses', function(flags) {
+    // like return Class.find(flags);
+});
