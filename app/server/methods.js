e414a9732552997631b60270be64cf69959ed063
Alexandre C. Gerona <acgerona@up.edu.ph>
2016-03-25 17:25:39 +0800

    Remove comments from editView() meteor method

--- app/server/methods.js
+++ app/server/methods.js
@@ -35,28 +35,10 @@ Meteor.methods({
     },
 
     'editClass': function (classId, classToEdit) { //Contains two arguments: the ID of the class to edit and the details to update the class with
-        // check(classToEdit, { //Checks the validity of the types of passed variables. The use of this function will be dependent on the UI's implementation of editClass.
-        //     courseTitle: String,
-        //     semester: String,
-        //     lecturer: String,
-        //     students: [Schema.StudentSchema]
-        // });
-
-        /* Space to include future error type-checking and whatnots */
-
         Class.update({'_id' : classId}, {$set:classToEdit});
     },
 
     'editView': function (viewId, viewToEdit) { //Contains two arguments: the ID of the class to edit and the details to update the class with
-        // check(viewToEdit, { //Checks the validity of the types of passed variables. The use of this function will be dependent on the UI's implementation of editClass.
-        //     courseTitle: String,
-        //     lecturer: String,
-        //     section: String,
-        //     view: [Schema.ViewState]
-        // });
-
-        /* Space to include future error type-checking and whatnots */
-
         View.update({'_id' : viewId}, {$set:viewToEdit});
     }
 });
