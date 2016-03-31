1cf909af9288355c6928778c93840cc8b5976164
Matthew Marcos <matthewmarcos94@gmail.com>
2016-04-01 04:55:38 +0800

    Can detect whether or not teacher account or not

--- app/client/templates/application/accountAndclassCards/AccountAndClassCards.js
+++ app/client/templates/application/accountAndclassCards/AccountAndClassCards.js
@@ -1,62 +1,3 @@
-var accountsData = [{
-    name: 'Reginald Recario',
-    username: 'regrecario',
-    acounttype: 'Teacher'
-}, {
-    name: 'REgie Recario',
-    username: 'regrecario2',
-    acounttype: 'Teacher2'
-}, {
-    name: 'Yepyep',
-    username: 'Yepyep',
-    acounttype: 'admin'
-}, {
-    name: 'Yepyep',
-    username: 'Yepyep',
-    acounttype: 'admin'
-}, {
-    name: 'Yepyep',
-    username: 'Yepyep',
-    acounttype: 'admin'
-}, {
-    name: 'Yepyep',
-    username: 'Yepyep',
-    acounttype: 'admin'
-}, {
-    name: 'Yepyep',
-    username: 'Yepyep',
-    acounttype: 'admin'
-}];
-
-var courseData = [{
-    coursecode: 'CMSC128',
-    coursetitle: 'Introduction to Software Engineering'
-}, {
-    coursecode: 'CMSC132',
-    coursetitle: 'cOMPUTER aRCHTECTURE'
-}, {
-    coursecode: 'CMSC170',
-    coursetitle: 'Artifcial intelligence'
-}, {
-    coursecode: 'CMSC125',
-    coursetitle: 'operating system'
-},{
-    coursecode: 'CMSC141',
-    coursetitle: 'Stuff'
-},{
-    coursecode: 'CMSC142',
-    coursetitle: 'stuff'
-},{
-    coursecode: 'CMSC125',
-    coursetitle: 'operating system'
-},{
-    coursecode: 'CMSC125',
-    coursetitle: 'operating system'
-},{
-    coursecode: 'CMSC125',
-    coursetitle: 'operating system'
-}];
-
 /*****************************************************************************/
 /* AccountAndClassCards: Event Handlers */
 /*****************************************************************************/
@@ -67,27 +8,16 @@ Template.AccountAndClassCards.events({});
 /*****************************************************************************/
 Template.AccountAndClassCards.helpers({
     isTeacher: function() {
-        // return true;
-        var user = Meteor.user();
-        // console.log(user);
-        if(!!user || !!user.profile || !!user.profile.type) {
-            return ;
-        }
-        else {
-            return user.profile.type === 'Teacher';
-        }
+
+        var type = Meteor.users.findOne({_id: Meteor.userId()}).profile.type;
+
+        return !!(type === 'Teacher');
     },
     isAdmin: function() {
 
-        var user = Meteor.user();
+        var type = Meteor.users.findOne({_id: Meteor.userId()}).profile.type;
 
-        if(!!user && !!user.profile && !!user.profile.type) {
-            return false;
-        }
-        else {
-            return user.profile.type === 'Admin';
-        }
-
+        return !!(type === 'Admin');
     },
     class: function () {
         return Class.find();
