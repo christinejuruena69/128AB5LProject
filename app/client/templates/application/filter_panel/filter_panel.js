f72b6ec7939c32b6607407ab06176a8f0bcb062d
dejesuscarl <carladriandejesus@gmail.com>
2016-04-01 21:15:15 +0800

    updated the filter panel

--- app/client/templates/application/filter_panel/filter_panel.js
+++ app/client/templates/application/filter_panel/filter_panel.js
@@ -1,7 +1,29 @@
 /*****************************************************************************/
 /* Home: Event Handlers */
 /*****************************************************************************/
-Template.filterPanel.events({});
+Template.filterPanel.events({
+	'click #selectAll': function (){
+		checkboxes = $('.labSection');
+		if(document.getElementById('selectAll').checked == true){
+			  for(var i=0, n=checkboxes.length;i<n;i++) {
+			  	checkboxes[i].name = i;
+			    checkboxes[i].checked = true;
+			  }
+			return;
+		}
+	}
+	
+	'click #custom': function (){
+		checkboxes = $('.labSection');
+		if(document.getElementById('custom').checked == true){
+			  for(var i=0, n=checkboxes.length;i<n;i++) {
+			  	checkboxes[i].checked = false;
+			  	checkboxes[i].name = "labSection";
+			  }
+			return;
+		}
+	}
+});
 /*****************************************************************************/
 /* Home: Helpers */
 /*****************************************************************************/
