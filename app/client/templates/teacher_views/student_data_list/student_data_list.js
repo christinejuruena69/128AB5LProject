01ec3c5384f5141c4e6e756e14911646c48b9877
Matthew Marcos <matthewmarcos94@gmail.com>
2016-04-01 09:23:28 +0800

    Changed some filenames and added them to matching folders

---
+++ app/client/templates/teacher_views/student_data_list/student_data_list.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* StudentDataList: Event Handlers */
+/*****************************************************************************/
+Template.StudentDataList.events({
+});
+
+/*****************************************************************************/
+/* StudentDataList: Helpers */
+/*****************************************************************************/
+Template.StudentDataList.helpers({
+});
+
+/*****************************************************************************/
+/* StudentDataList: Lifecycle Hooks */
+/*****************************************************************************/
+Template.StudentDataList.onCreated(function () {
+});
+
+Template.StudentDataList.onRendered(function () {
+});
+
+Template.StudentDataList.onDestroyed(function () {
+});
