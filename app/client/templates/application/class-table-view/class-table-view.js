878d445688e3cd1c0d5ea47d20cafaf5f2c9a5c2
christinejuruena69 <christinemaejuruena@gmail.com>
2016-04-02 21:35:52 +0800

    add form for add teacher. fixing other forms yet

---
+++ app/client/templates/application/class-table-view/class-table-view.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* ClassTableView: Event Handlers */
+/*****************************************************************************/
+Template.ClassTableView.events({
+});
+
+/*****************************************************************************/
+/* ClassTableView: Helpers */
+/*****************************************************************************/
+Template.ClassTableView.helpers({
+});
+
+/*****************************************************************************/
+/* ClassTableView: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ClassTableView.onCreated(function () {
+});
+
+Template.ClassTableView.onRendered(function () {
+});
+
+Template.ClassTableView.onDestroyed(function () {
+});
