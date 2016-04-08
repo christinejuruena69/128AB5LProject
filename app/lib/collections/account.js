8025b7598cfd150ba14b518cc7f0867432723eb0
lmlawas1 <leenseylawas2007@gmail.com>
2016-04-08 22:12:04 +0800

    Add update details for fullname, email, and password

---
+++ app/lib/collections/account.js
@@ -0,0 +1,55 @@
+Meteor.methods({
+
+    'User/UpdateDetails': function (userDetails) {
+
+        check(userDetails, {
+            fullname: String,
+            email: String,
+            oldPassword: String,
+            newPassword: String
+        });           
+
+        // change password
+        Accounts.changePassword(userDetails.oldPassword, userDetails.newPassword, function(error) {
+
+            if (error) {
+                throwError(error.reason);
+                console.log('Error');
+            }
+            else {      
+                Router.go('/');
+                console.log('Success');
+            }
+        });
+        
+        // change email
+        Meteor.users.update(
+            {'_id':Meteor.user()._id},
+            {$set:{'emails' : {'address': userDetails.email}}}
+        );
+
+        // change profile.fullName
+        Meteor.users.update(
+            {'_id':Meteor.user()._id},
+            {$set:{'profile':{'fullName': userDetails.fullname}}}
+        );
+    }
+});
+
+if (Meteor.isServer) {
+
+    Meteor.users.allow({
+
+        update: function (userId, doc) {
+
+            var user = Meteor.users.findOne({ '_id': userId });
+
+            if( user._id === Meteor.userId() ){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    });
+}
