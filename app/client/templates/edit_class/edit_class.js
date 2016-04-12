6c6a2dff8605d8e25a182397a705026b635a2e5d
altusgerona <alecgerona@gmail.com>
2016-04-13 00:11:04 +0800

    Make the editClass event call the editClass() meteor method

--- app/client/templates/edit_class/edit_class.js
+++ app/client/templates/edit_class/edit_class.js
@@ -2,6 +2,28 @@
 /* EditClass: Event Handlers */
 /*****************************************************************************/
 Template.EditClass.events({
+    'submit form': function (event, template) {
+        console.log('Submitted');
+        event.preventDefault();
+        // Stop html from going to action
+
+        //Get form data
+        var editedClass = {},
+            courseTitle = event.target['courseTitle'].value,
+            courseCode = event.target['courseCode'].value,
+            semester = event.target['semester'].value;
+
+        Meteor.call('User/editClass', editedClass, function(error, result){
+
+            // alert if error
+            if(error){
+                return throwError(error.reason);
+            }
+
+            // re-route to home if success
+            Router.go('/');
+        });
+    }
 });
 
 /*****************************************************************************/
