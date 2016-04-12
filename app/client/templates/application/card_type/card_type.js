676e752d18a92f347dfee5ce86de057493bdb9d7
lmlawas1 <leenseylawas2007@gmail.com>
2016-04-12 16:44:00 +0800

    Route + button to add-class route

--- app/client/templates/application/card_type/card_type.js
+++ app/client/templates/application/card_type/card_type.js
@@ -1,10 +1,14 @@
-Template.CardType.events({});
+Template.CardType.events({
+    'click #addClass': function() {
+        Router.go('/add-class');
+    }
+});
 
 Template.CardType.helpers({
     dataType: function() {
 
-        var type = Meteor.users.findOne({_id: Meteor.userId()}).profile.type;
+        var type = Meteor.users.findOne({ _id: Meteor.userId() }).profile.type;
 
-        return (type === 'Teacher')? 'Classes': 'Accounts';
+        return (type === 'Teacher') ? 'Classes' : 'Accounts';
     }
 });
