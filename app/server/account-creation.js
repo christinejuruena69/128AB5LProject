4da32b00827f788fd8ac252b42d9a2db476f3580
Matthew Marcos <matthewmarcos94@gmail.com>
2016-04-20 11:03:38 +0800

    Block auto sign in on account creation

--- app/server/account-creation.js
+++ app/server/account-creation.js
@@ -1,10 +1,22 @@
 Accounts.onCreateUser(function (options, user) {
     // Use provided profile in options, or create an empty object
     user.profile = options.profile || {};
-
     // Assigns first and last names to the newly created user object
     user.profile.fullName = options.fullName;
     user.profile.type = options.type;
     // Returns the user object
     return user;
+});
+
+Accounts.validateLoginAttempt(function (attemptInfo) {
+// Block auto login attempt after signup
+    if (attemptInfo.type == 'resume') {
+        return true;
+    }
+
+    if (attemptInfo.methodName == 'createUser') {
+        return false;
+    }
+
+    return true;
 });
