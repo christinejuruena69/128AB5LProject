5a71a3dc987f80155fa00b3eb7f7c2aa1412853b
Matthew Marcos <matthewmarcos94@gmail.com>
2016-04-20 13:50:59 +0800

    Added notification

--- app/client/collections/notification.js
+++ app/client/collections/notification.js
@@ -1,6 +1,10 @@
 // Local (client-only) collection
 Notification = new Mongo.Collection(null);
 
-notify = function (message) {
-    Notification.insert({ message })
-};
+
+notify = function(message, type) {
+    // class = good or bad
+    var type = (type === 'good')? 'alert-success' : 'alert-danger';
+
+    Notification.insert({ message, class: type });
+}
