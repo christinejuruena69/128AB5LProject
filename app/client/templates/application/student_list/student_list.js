be5198a8ce7d5f0e1de78593d27b4469a76cf7b6
jpaobandong <jpaobandong@gmail.com>
2016-04-24 22:49:44 +0800

    fixed edit student and jquery html data fetching

--- app/client/templates/application/student_list/student_list.js
+++ app/client/templates/application/student_list/student_list.js
@@ -8,25 +8,6 @@ Template.StudentList.events({
     },
     'click .down': function() {
         $('.spinner input').val(parseInt($('.spinner input').val(), 10) - 1);
-    },
-    'click .editclass': function() {
-        var table = document.getElementById("student-table");
-        if (table != null) {
-            for (var i = 0; i < table.rows.length; i++) {
-                table.rows[i].onclick = function() {
-                    tableText(this);
-                };
-            }
-        }
-
-        function tableText(tableCell) {
-            document.getElementById("modal-full-name").innerHTML = tableCell.cells[1].innerHTML;
-            document.getElementById("modal-std-no").innerHTML = tableCell.cells[0].innerHTML;
-            document.getElementById("modal-nickname").value = tableCell.cells[2].innerHTML;
-            document.getElementById("modal-course").value = tableCell.cells[3].innerHTML;
-            document.getElementById("modal-college").value = tableCell.cells[4].innerHTML;
-            document.getElementById("modal-bias").value = tableCell.cells[6].innerHTML;
-        }
     }
 });
 
