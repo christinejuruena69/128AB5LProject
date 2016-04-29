9b4f0adcd6e34f0d2c8b79368ecf9a4e07c3f843
jpaobandong <jpaobandong@gmail.com>
2016-04-29 16:44:31 +0800

    added prompts

--- app/client/templates/application/account_card/account_card.js
+++ app/client/templates/application/account_card/account_card.js
@@ -2,12 +2,23 @@ Template.AccountCard.helpers({
 
 });
 Template.AccountCard.events({
+    'click #deleteAccount': function() {
+        var message = "Delete " + this.profile.fullName + "?";
 
+        var verificationPrompt1 = confirm(message);
+        if(verificationPrompt1 == true){
+            var verificationPrompt2 = confirm("Deleting a Teacher Account will also delete classes. \nAre you sure you want to delete?");
+            if (verificationPrompt2 == true) {
+                
+            };
+        }
+    }
 });
 /*****************************************************************************/
 /* AccountCard: Lifecycle Hooks */
 /*****************************************************************************/
 Template.AccountCard.onCreated(function () {
+    Meteor.subscribe('teacherClasses');
 });
 Template.AccountCard.onRendered(function () {});
 Template.AccountCard.onDestroyed(function () {});
