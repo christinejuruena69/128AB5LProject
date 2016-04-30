e93a787cd74853f6ac7d894f7919e7e8781904b4
Matthew Marcos <matthewmarcos94@gmail.com>
2016-04-30 10:18:50 +0800

    Admin can edit classes. Teacher can only edit his/her own class

--- app/client/templates/edit_class/edit_class.js
+++ app/client/templates/edit_class/edit_class.js
@@ -3,31 +3,61 @@
 /*****************************************************************************/
 Template.EditClass.events({
     'submit form': function (e, template) {
-        event.preventDefault();
+        e.preventDefault();
 
-        //Get form data
+        /****************************************************/
+        /*  Do not get the form data by ID as there are     */
+        /*  already nume numerous forms with the same ID    */
+        /****************************************************/
+
         var editedClass = {
             courseTitle: $(e.target).find('[name=courseTitle]').val(),
             courseCode: $(e.target).find('[name=courseCode]').val(),
             semester: $(e.target).find('[name=semester]').val(),
+            section: $(e.target).find('[name=section]').val(),
+            lecturer: this.lecturer,
             _id: this._id
         };
 
+        if (courseTitle === '') {
+            return notify('Course title is required', 'bad');
+        }
+        else if (courseCode === '') {
+            return notify('Course code is required', 'bad');
+        }
+        else if (semester === '') {
+            return notify('Semester is required', 'bad');
+        }
+        else if (section === '') {
+            return notify('Section is required', 'bad');
+        }
+
+        /****************************************************/
+        /*  Add REGEXES Here to match with form INPUT     */
+        /****************************************************/
+
         Meteor.call('User/editClass', editedClass, function(error, result){
 
-            if(error){
-                notify(error.reason, 'bad');
-                return throwError(error.reason);
+            if(error) {
+                return notify(error.reason, 'bad');
             }
-
-            var successNotif = 'Successfully edited '
+            else {
+                var successNotif = 'Successfully edited '
                 + editedClass.courseCode + ': '
                 + editedClass.courseTitle;
 
-            notify(successNotif, 'good');
+                notify(successNotif, 'good');
 
-            $('#admin-modal-editclass-' + editedClass._id).modal('hide');
+                $('#admin-modal-editclass-' + editedClass._id).modal('hide');
+
+            }
         });
+    },
+
+    'click #cancel': function (e, template) {
+
+        e.preventDefault();
+        $('#admin-modal-editclass-' + editedClass._id).modal('hide');
     }
 });
 
